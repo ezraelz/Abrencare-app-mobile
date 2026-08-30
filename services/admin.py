c6c000d12dd966cbc5db89dbbb65483557af51cb
esrael <esraelzerihun50@gmail.com>
2026-08-30 23:12:02 +0300

    refined service module

--- services/admin.py
+++ services/admin.py
@@ -3,15 +3,14 @@ from .models import Service, Feature
 
 @admin.register(Feature)
 class FeatureAdmin(admin.ModelAdmin):
-    list_display = ['id', 'feature_name', 'description', 'created_at']
-    search_fields = ['feature_name']
+    list_display = ['id', 'name', 'description', 'created_at']
+    search_fields = ['name']
     list_filter = ['created_at']
     ordering = ['id']
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ['id', 'service_name', 'service_for', 'description', 'created_at']
-    search_fields = ['service_name', 'service_for']
+    list_display = ['id', 'name', 'service_for', 'description', 'created_at']
+    search_fields = ['name', 'service_for']
     list_filter = ['created_at']
     ordering = ['id']
-    filter_horizontal = ['features']
